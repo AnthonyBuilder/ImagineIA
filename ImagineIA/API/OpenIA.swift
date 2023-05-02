404df35ecfda55a03de419f0d4216555079216d3
Anthony José <anthonyJ2017@icloud.com>
2023-05-02 19:28:38 -0300

    update

--- ImagineIA/API/OpenIA.swift
+++ ImagineIA/API/OpenIA.swift
@@ -7,42 +7,41 @@
 
 import Foundation
 
-class APIOpenIA: ObservableObject {
-    
-    func generateImage(image prompt: String, number: Int, completion: @escaping (Result<OpenIAModel, OpenIAError>) -> ()) async {
-        // request on openIA API with type image generate2
-        let body: [String: Any] = ["prompt": prompt, "n": number, "size": "1024x1024"]
-        let finalBody = try? JSONSerialization.data(withJSONObject: body)
+    class APIOpenIA: ObservableObject {
         
-        guard let url = URL(string: "https://api.openai.com/v1/images/generations") else {
-             return
-         }
-        
-        var request = URLRequest(url: url)
-        
-        request.setValue("Bearer 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", forHTTPHeaderField: "Authorization")
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpMethod = "POST"
-        request.httpBody = finalBody
+        func generateImage(image prompt: String, number: Int, completion: @escaping (Result<OpenIAModel, OpenIAError>) -> ()) async {
+            // request on openIA API with type image generate2
+            let body: [String: Any] = ["prompt": prompt, "n": number, "size": "1024x1024"]
+            let finalBody = try? JSONSerialization.data(withJSONObject: body)
+            
+            guard let url = URL(string: "https://api.openai.com/v1/images/generations") else {
+                 return
+             }
+            
+            var request = URLRequest(url: url)
+            
+            request.setValue("Bearer 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", forHTTPHeaderField: "Authorization")
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpMethod = "POST"
+            request.httpBody = finalBody
 
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            DispatchQueue.main.async {
-                Task {
-                    print(String(decoding: data!, as: UTF8.self))
-                    do {
-                        let error_decoded = try JSONDecoder().decode(OpenIAError.self, from: data!)
-                        completion(.failure(error_decoded))
-                    } catch {
-                        let data_decoded = try! JSONDecoder().decode(OpenIAModel.self, from: data!)
-                        completion(.success(data_decoded))
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                DispatchQueue.main.async {
+                    Task {
+                        print(String(decoding: data!, as: UTF8.self))
+                        do {
+                            let error_decoded = try JSONDecoder().decode(OpenIAError.self, from: data!)
+                            completion(.failure(error_decoded))
+                        } catch {
+                            let data_decoded = try! JSONDecoder().decode(OpenIAModel.self, from: data!)
+                            completion(.success(data_decoded))
+                        }
                     }
                 }
-            }
-        }.resume()
-        
+            }.resume()
+        }
     }
-}
 
 // MARK: - OpenIAModel
 struct OpenIAModel: Codable {
